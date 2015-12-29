7f70899c30e3118bb109233d60473f88739b6c43
rilkevb <rilkevb@gmail.com>
2015-12-29 13:20:18 -0800

    tests run.

--- test/index.js
+++ test/index.js
@@ -1,31 +1,41 @@
 // var assert = require('assert')
 // var React = require('react/addons')
-var assert = require('assert')
-var SimpleFormat = require('../src/index')
-var ReactTestUtils = require('react-addons-test-utils');
+// var assert = require('assert')
+// var SimpleFormat = require('../src/index')
+// var ReactTestUtils = require('react-addons-test-utils');
+import React from 'react'
+import SimpleFormat from '../src/index'
+import TestUtils from 'react-addons-test-utils'
+import { expect } from 'chai'
 
-describe('simple-format', function() {
+describe('SimpleFormat', () => {
+  it('renders a pair of buttons', () => {
+  })
   it('should work!', function() {
-    expect(true).to.be.true;
-  });
-});
+    let state = 42
+    expect(state).to.equal(42)
+  })
+})
 
+describe('simple-format component rendering string with no newlines', function() {
 
-// describe('simple-format component rendering string with no newlines', function() {
+  before('render and locate element', function() {
+    var renderedComponent = TestUtils.renderIntoDocument(
+      <SimpleFormat text={ "You cannot connect the dots looking forward; You can only connect them looking backward." } />
+    )
 
-//   before('render and locate element', function() {
-//     var renderedComponent = TestUtils.renderIntoDocument(
-//       <SimpleFormat text={ "You cannot connect the dots looking forward; You can only connect them looking backward." } />
-//     )
+    var inputComponent = TestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'div')
+    this.inputElement = inputComponent //.getDOMNode()
+  })
 
-//     var inputComponent = TestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'div')
-//     this.inputElement = inputComponent.getDOMNode()
-//   })
-
-//   it('renders strings without newlines as a single paragraph', function() {
-//     assert(this.inputElement === '<div><p>You cannot connect the dots looking forward; You can only connect them looking backward</p></div>')
-//   })
-// })
+  it('should work!', function() {
+  })
+  console.log(this.inputElement)
+  it(this.inputElement, function() {
+    expect(this.inputElement).to.equal('yo, a wicked cup of coco')
+    // '<div><p>You cannot connect the dots looking forward; You can only connect them looking backward</p></div>'
+  })
+})
 
 
   //     assert_equal "<p></p>", simple_format(nil)
--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,14 @@
-var jsdom = require('jsdom')
+import jsdom from 'jsdom'
+import chai from 'chai'
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
-global.window = document.parentWindow
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+const win = doc.defaultView
+
+global.document = doc
+global.window = win
+
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key]
+  }
+})
